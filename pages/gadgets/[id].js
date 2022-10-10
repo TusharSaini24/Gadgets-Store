c39de9a0b5cbb5d396e4002258b798969cc4e261
TusharSaini24 <sainitushar239@gmail.com>
2022-10-10 18:28:38 +0530

    changes

--- pages/gadgets/[id].js
+++ pages/gadgets/[id].js
@@ -59,10 +59,10 @@ const Gadget = (props) => {
               click to see props
             </button>
             <h2 className="text-sm title-font text-gray-500 tracking-widest">
-              {propGadget.name}
+              {propGadget?.name ?? "nameNotDefined"}
             </h2>
             <h1 className="text-gray-900 text-3xl title-font font-medium mb-4">
-              {propGadget.description}
+              {propGadget?.description ?? "descriptionNotDefined"}
             </h1>
             <div className="flex mb-4">
               <a className="flex-grow text-green-500 border-b-2 border-green-500 py-2 text-lg px-1">
@@ -76,7 +76,7 @@ const Gadget = (props) => {
               </a> */}
             </div>
             <p className="leading-relaxed mb-4">
-              {propGadget.description}
+              {/* {propGadget.description} */}
               <br />
               Fam locavore kickstarter distillery. Mixtape chillwave tumeric
               sriracha taximy chia microdosing tilde DIY. XOXO fam inxigo
@@ -88,7 +88,7 @@ const Gadget = (props) => {
             <div className="flex border-t border-b mb-6 border-gray-200 py-2">
               <span className="text-gray-500">Quantity</span>
               <span className="ml-auto text-gray-900">
-                {propGadget.countInStock}
+                {/* {propGadget.countInStock} */}
               </span>
             </div>
             <div className="flex my-2 items-center">
@@ -98,9 +98,9 @@ const Gadget = (props) => {
                   className="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10"
                   onChange={(e) => setQantity(e.target.value)}
                 >
-                  {[...Array(propGadget.countInStock).keys()].map((x) => (
+                  {/* {[...Array(propGadget.countInStock).keys()].map((x) => (
                     <option key={x + 1}>{x + 1}</option>
-                  ))}
+                  ))} */}
                 </select>
                 <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
                   <svg
@@ -119,7 +119,7 @@ const Gadget = (props) => {
             </div>
             <div className="flex">
               <span className="title-font font-medium text-2xl text-gray-900">
-                &#x20B9; {propGadget.price}
+                {/* &#x20B9; {propGadget.price} */}
               </span>
               {/* <Link href={`gadgets/cart/${propGadget._id}`}> */}
               <button
@@ -143,11 +143,11 @@ const Gadget = (props) => {
               </button>
             </div>
           </div>
-          <img
+          {/* <img
             alt="ecommerce"
             className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
             src={propGadget.image}
-          />
+          /> */}
         </div>
       </div>
     </section>
